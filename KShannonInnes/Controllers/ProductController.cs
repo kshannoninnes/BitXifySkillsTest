2269353fa8805a0fb5255844a9867d351afe5483
kalon <kalon@DESKTOP-AKTHBQN>
2026-01-02 03:00:40 +0800

    Add project files.

---
+++ KShannonInnes/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using KShannonInnes;
+using KShannonInnes.Models;
+
+public class ProductController : Controller
+{
+    [HttpGet]
+    public JsonResult GetAll()
+    {
+        var connectionString =
+            ConfigurationManager.ConnectionStrings["BitXifySampleTestDBConnectionString"].ConnectionString;
+
+        using (var db = new BitXifyDataContext(connectionString))
+        {
+            var products = db.tblProducts
+                .Select(product => new Product { Name = product.Name, Price = product.Price })
+                .ToList();
+
+            // Apparently Json wasn't allowed for GET requests due to an exploit in early browsers
+            // called Json Hijacking. This is fixed in modern browsers so is largely a non-issue.
+            // JsonREquestBehaviour tells asp.net to allow the json return.
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ KShannonInnes/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KShannonInnes.Models
+{
+    public sealed class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
